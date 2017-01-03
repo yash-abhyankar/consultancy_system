c512e9883ff57fcc73d875f4c8b0d59f795d733f
yash-abhyankar <yashabhyankar95@gmail.com>
2017-01-03 14:04:52 +0530

    First commit

--- app/scripts/directives/sidebar/sidebar.js
+++ app/scripts/directives/sidebar/sidebar.js
@@ -8,33 +8,38 @@
  */
 
 angular.module('sbAdminApp')
-  .directive('sidebar',['$location',function() {
-    return {
-      templateUrl:'scripts/directives/sidebar/sidebar.html',
-      restrict: 'E',
-      replace: true,
-      scope: {
-      },
-      controller:function($scope){
-        $scope.selectedMenu = 'dashboard';
-        $scope.collapseVar = 0;
-        $scope.multiCollapseVar = 0;
-        
-        $scope.check = function(x){
-          
-          if(x==$scope.collapseVar)
-            $scope.collapseVar = 0;
-          else
-            $scope.collapseVar = x;
-        };
-        
-        $scope.multiCheck = function(y){
-          
-          if(y==$scope.multiCollapseVar)
-            $scope.multiCollapseVar = 0;
-          else
-            $scope.multiCollapseVar = y;
-        };
+  .directive('sidebar', ['$location', function () {
+      return {
+          templateUrl: 'scripts/directives/sidebar/sidebar.html',
+          restrict: 'E',
+          replace: true,
+          scope: {
+          },
+          controller: function ($scope) {
+              $scope.selectedMenu = 'dashboard';
+              $scope.collapseVar = 0;
+              $scope.multiCollapseVar = 0;
+
+              $scope.check = function (x) {
+
+                  if (x == $scope.collapseVar)
+                      $scope.collapseVar = 0;
+                  else
+                      $scope.collapseVar = x;
+              };
+
+              $scope.multiCheck = function (y) {
+
+                  if (y == $scope.multiCollapseVar)
+                      $scope.multiCollapseVar = 0;
+                  else
+                      $scope.multiCollapseVar = y;
+              };
+
+              //fetch latest jd (3 max)
+              $scope.jobs = [{ id: 1, position: 'Cheif Technology Officer', status: 'Open' }, { id: 2, position: 'UI/UX Designer', status: 'Open' }, { id: 3, position: 'Java Developer', status: 'Close' }];
+
+          }
+
       }
-    }
   }]);
